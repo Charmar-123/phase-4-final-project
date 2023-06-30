5ad513b45f97d45bf179ee25e678db9a494ca4fc
Charbel Maroun <charbel-maroun@hotmail.com>
2023-06-30 19:28:38 -0400

    create doctor card

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
-
+import ActionAreaCard from './components/DoctorCard';
 import './App.css';
 import { useEffect, useState } from 'react';
 import Doctors from './components/Doctors';
+import DoctorCard from './components/DoctorCard';
+
 
 const App = () => {
 
-  const [doctors, setDoctors] = useState([]);
+  const [doctorsData, setDoctorsData] = useState([]);
 
   useEffect(() => {
     fetch('/doctors')
-    .then(res => res.json())
-    .then(setDoctors)
+      .then(res => res.json())
+      .then(setDoctorsData)
   }, [])
   return (
-   <Doctors />
+    <>
+      <Doctors doctorsData={doctorsData} />
+    </>
+
   );
 }
 
